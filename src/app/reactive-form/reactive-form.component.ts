06dd6ccd344b5d098d9c03fdbe048ca93151acf1
varshithrajbasa <varshithraj.basa@gmail.com>
2022-02-05 17:48:35 +0530

    Added Custom Validator

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -4,38 +4,45 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.css']
+  styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
   signUpForm: FormGroup;
-  genders=['male','female'];
-  constructor() { }
+  forbiddenUserNames = ['admin', 'superuser'];
+  genders = ['male', 'female'];
+  constructor() {}
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
-      'userData':new FormGroup({
-        'username':new FormControl(null,Validators.required),
-        'email':new FormControl(null,[Validators.required,Validators.email])
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
       }),
-      'gender':new FormControl('male'),
-      'hobbies':new FormArray([])
+      gender: new FormControl('male'),
+      hobbies: new FormArray([]),
     });
   }
   submitForm() {
     console.log(this.signUpForm.value);
-
   }
   setDefault(e) {
     e.preventDefault();
     this.signUpForm.patchValue({
-      userData:{username: 'test',
-      email: 'test@test.com'},
-      gender:'male'
+      userData: { username: 'test', email: 'test@test.com' },
+      gender: 'male',
     });
   }
-  onAddHobby(){
+  onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
-
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return { nameIsForBidden: true };
+    }
+    return {};
+  }
 }
