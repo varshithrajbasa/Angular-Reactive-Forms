e05b2ad2e9defd65d0e2dcc29508e23e929f859a
varshithrajbasa <varshithraj.basa@gmail.com>
2022-02-05 13:57:01 +0530

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, ReactiveFormsModule ],
+  declarations: [ AppComponent, ReactiveFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
-
+  signUpForm: FormGroup;
+  genders=['male','female'];
   constructor() { }
 
   ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'userData':new FormGroup({
+        'username':new FormControl(null,Validators.required),
+        'email':new FormControl(null,[Validators.required,Validators.email])
+      }),
+      'gender':new FormControl('male'),
+      'hobbies':new FormArray([])
+    });
+  }
+  submitForm() {
+    console.log(this.signUpForm.value);
+
+  }
+  setDefault(e) {
+    e.preventDefault();
+    this.signUpForm.patchValue({
+      userData:{username: 'test',
+      email: 'test@test.com'},
+      gender:'male'
+    });
+  }
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
 
 }
